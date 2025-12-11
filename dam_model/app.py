b063fcd5437ff85775c4537a59e839912125b83e
Shaikh Irafan <s23_shaikh_irafan@mgmcen.ac.in>
2025-12-11 15:00:59 +0530

    Add Files

--- dam_model/app.py
+++ dam_model/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -31,4 +32,5 @@ def predict():
     return jsonify({"probability": proba, "overflow_label": label})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
